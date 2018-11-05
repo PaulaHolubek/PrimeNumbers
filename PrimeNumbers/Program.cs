8e75459884a3e8acc1bf25323b57c709685ec2c4
Paula H <42469219+PaulaHolubek@users.noreply.github.com>
2018-11-05 12:14:02 -0500

    Add files via upload

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 2; counter < 100; counter++)
+            {
+                int primes = 0;
+                for (int y = 1; y < counter; y++)
+                {
+                    if (counter % y == 0)
+                        primes++;
+
+                    if (primes == 2) break;
+                }
+                if (primes != 2)
+                    Console.WriteLine(counter);
+
+                primes = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
